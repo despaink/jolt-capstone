e7271d48985cd37b911d53ab2659bae26e1442a1
decongo <conger.devon@gmail.com>
2020-02-11 10:22:30 -0700

    fixed results path to include date

--- template-athena-service/daily.py
+++ template-athena-service/daily.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 import logging
-from datetime import date
+from datetime import date, timedelta
 
 athena_client = boto3.client('athena')
 
@@ -9,7 +9,7 @@ athena_client = boto3.client('athena')
 def handle(event, context):
     # extract these from event
     storeName = 'store_name_1'  
-    today = date.today().strftime('%Y-%m-%d')
+    today = ( date.today() - timedelta(days=1) ).strftime('%Y-%m-%d')
 
     baseOutputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}'
 
@@ -19,10 +19,11 @@ def handle(event, context):
 
 
 def uniquePerHour(today, baseInputLocation):
+    # WHERE clause is hard-coded with the only day we have data for
     athenaQuery = (
         "SELECT date_trunc('hour', first_seen) time, Count(*) visits "
 		"FROM store_name_1 "
-        f"WHERE DATE(first_seen)=DATE('{today}') "
+        f"WHERE DATE(first_seen)=DATE('2019-12-12') "
 		"GROUP BY date_trunc('hour', first_seen) "
 		"ORDER BY date_trunc('hour', first_seen)"
         )
