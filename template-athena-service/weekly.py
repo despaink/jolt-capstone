dd02dc74ab981c1e5ee107e70d8761287e1691e1
decongo <conger.devon@gmail.com>
2020-02-11 11:30:22 -0700

    began unique per day implementation

--- template-athena-service/weekly.py
+++ template-athena-service/weekly.py
@@ -1,21 +1,39 @@
 import json
 import boto3
 import logging
+from datetime import date
 
 athena_client = boto3.client('athena')
 
 # triggered at 5am every Sunday
 def handle(event, context):
-    #        QueryString="select created interval, count(*) count from (  select substr(to_char(first_seen, 'hh24:mi'), 1, 4) || '0' created from test_timestamp ) group by created order by created;",
+    # TODO: extract storeName from event
+    storeName = 'store_name_1'
+    day = date.today()
 
-    response = athena_client.start_query_execution(
-        QueryString="SELECT mac FROM capstone.scan_data limit 10;",
-        QueryExecutionContext={
-            'Database': 'capstone'
-        },
-        ResultConfiguration={
-            'OutputLocation': 's3://jolt.capstone/athena-query-logs/dtest/weekly',
-        }
-    )
+    baseOutputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}'
+
+    response = uniquePerDay(storeName, day, baseOutputLocation)
     print(response)   
     return response 
+
+
+def uniquePerDay(storeName, day, baseOutputLocation): # not completed
+    weekStart = ''
+    weekEnd = ''
+    athenaQuery = (
+        "SELECT date_trunc('day', first_seen) time, Count(*) visits "
+		f"FROM {storeName} "
+        f"WHERE DATE(first_seen) BETWEEN DATE({weekStart}) AND DATE({weekEnd})"
+		"GROUP BY date_trunc('day', first_seen) "
+		"ORDER BY date_trunc('day', first_seen)"
+    )
+
+    outputLocation = f'{baseOutputLocation}/unique_per_hour/{day}'
+
+    response = athena_client.start_query_execution(
+        QueryString = athenaQuery,
+        QueryExecutionContext = { 'Database': 'capstone' },
+        ResultConfiguration = { 'OutputLocation': outputLocation }
+    )
+    return response
