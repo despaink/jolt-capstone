2729c5074f2c02dd4e65b9d2fb1f9366c4f30558
decongo <conger.devon@gmail.com>
2020-02-19 20:29:30 -0700

    totalUnique, repeatByMac coded

--- template-athena-service/daily.py
+++ template-athena-service/daily.py
@@ -11,27 +11,53 @@ def handle(event, context):
     storeName = 'store_name_1'  
     day = ( date.today() - timedelta(1) ).strftime('%Y-%m-%d')
 
-    baseOutputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}'
-
-    response = uniquePerHour(storeName, day, baseOutputLocation)
+    response = uniquePerHour(storeName, day)
     print(response)
     return response
 
 
-def uniquePerHour(storeName, day, baseOutputLocation):
-    athenaQuery = (
+def executeQuery(query, outputLocation):
+    return athena_client.start_query_execution(
+        QueryString = query,
+        QueryExecutionContext = { 'Database': 'capstone' },
+        ResultConfiguration = { 'OutputLocation': outputLocation }
+    )
+
+
+def constructOutputLocation(storeName, queryName, day):
+    return f's3://jolt.capstone/athena-query-logs/{storeName}/{queryName}/{day}'
+
+
+def uniquePerHour(storeName, day):
+    query = (
         "SELECT date_trunc('hour', first_seen) time, Count(*) visits "
 		f"FROM {storeName} "
         "WHERE DATE(first_seen)=DATE('${day}') "
 		"GROUP BY date_trunc('hour', first_seen) "
 		"ORDER BY date_trunc('hour', first_seen)"
     )
-    
-    outputLocation = f'{baseOutputLocation}/unique_per_hour/{day}'
+    outputLocation = constructOutputLocation(storeName, 'unique_per_hour', day)
+    return executeQuery(query, outputLocation)
 
-    response = athena_client.start_query_execution(
-        QueryString = athenaQuery,
-        QueryExecutionContext = { 'Database': 'capstone' },
-        ResultConfiguration = { 'OutputLocation': outputLocation }
+
+def totalUnique(storeName, day, baseOutputLocation):
+    query = (
+        "SELECT COUNT(DISTINCT mac) unique_devices "
+        f"FROM {storeName} "
+        f"WHERE DATE(first_seen)=DATE('{day}')"
     )
-    return response 
+    outputLocation = constructOutputLocation(storeName, 'total_unique', day)
+    return executeQuery(query, outputLocation)
+
+
+def repeatByMac(storeName, day, baseOutputLocation):
+    query = (
+        "SELECT mac, COUNT(*) visits "
+        f"FROM {storeName} "
+        f"WHERE DATE(first_seen)=DATE('{day}') "
+        "GROUP BY mac "
+        "HAVING COUNT(*) > 1 "
+        "ORDER BY COUNT(*) DESC"
+    )
+    outputLocation = constructOutputLocation(storeName, 'repeat_by_mac', day)
+    return executeQuery(query, outputLocation)
