a3f15f62e527e488020c3c16bf503d388f953deb
decongo <conger.devon@gmail.com>
2020-03-17 15:02:48 -0600

    join and partition openCVS compliant

--- template-athena-service/daily.py
+++ template-athena-service/daily.py
@@ -8,16 +8,16 @@ athena_client = boto3.client('athena')
 # triggered at 5am every morning
 def handle(event, context):
     # extract storeName from event?
-    storeName = 'store_name_1'
+    storeName = 'heritage_15'
     day = ( date.today() - timedelta(1) ).strftime('%Y-%m-%d')
     
     responses = []
     responses.append(joinDailyRecords(storeName, day))
     responses.append(addPartition(storeName, day))
-    responses.append(uniquePerHour(storeName, day))
-    responses.append(totalUnique(storeName, day))
-    responses.append(repeatByMac(storeName, day))
-    responses.append(averageVisitDurationInMinutes(storeName, day))
+    # responses.append(uniquePerHour(storeName, day))
+    # responses.append(totalUnique(storeName, day))
+    # responses.append(repeatByMac(storeName, day))
+    # responses.append(averageVisitDurationInMinutes(storeName, day))
     
     print(responses)
     return responses
@@ -43,17 +43,18 @@ def constructOutputLocation(storeName, queryName, day):
 # # # # # # # # # # # # # # # # # # 
 def joinDailyRecords(storeName, day): 
     query = (
-        "SELECT mac, min(first_seen) first_seen, max(last_seen) last_seen, power "
-        f"FROM {storeName} "
-        f"WHERE date(first_seen)=date('{day}') and date(last_seen)=date('{day}') "
-        "GROUP BY mac, power"
+        "SELECT mac, min(first_seen) first_seen, max(last_seen) last_seen, min(power) power "
+        f"FROM {storeName}_intermediate "
+        f"WHERE date(date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s'))=date('{day}') "
+        f"and date(date_parse(trim(last_seen), '%Y-%m-%d %H:%i:%s'))=date('{day}') "
+        "GROUP BY mac"
     )
-    outputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}/intermediate/dt={day}/daily_joined/'
+    outputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}/intermediate/dt={day}/'
     return executeQuery(query, outputLocation)
 
 
 def addPartition(storeName, day):
-    query = f"ALTER TABLE capstone.{storeName}_intermediate ADD IF NOT EXISTS PARTITION (dt='{day}') location 's3://jolt.capstone/athena-query-logs/{storeName}_intermediate/'"
+    query = f"ALTER TABLE capstone.{storeName} ADD IF NOT EXISTS PARTITION (dt='{day}')"
     outputLocation = f's3://jolt.capstone/athena-query-logs/{storeName}/partition-logs/dt={day}/'
     return executeQuery(query, outputLocation)
 
@@ -61,8 +62,8 @@ def addPartition(storeName, day):
 def uniquePerHour(storeName, day):
     query = (
         "SELECT date_trunc('hour', first_seen) time, Count(*) visits "
-		f"FROM {storeName}_intermediate "
-        f"WHERE dt=DATE('{day}') "
+		f"FROM {storeName} "
+        f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata' "
 		"GROUP BY date_trunc('hour', first_seen) "
 		"ORDER BY date_trunc('hour', first_seen)"
     )
@@ -75,7 +76,7 @@ def totalUnique(storeName, day):
     query = (
         "SELECT COUNT(DISTINCT mac) visits "
         f"FROM {storeName}_intermediate "
-        f"WHERE dt=DATE('{day}')"
+        f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'daily_total_unique', day)
     return executeQuery(query, outputLocation)
@@ -85,7 +86,7 @@ def repeatByMac(storeName, day):
     query = (
         "SELECT mac, COUNT(*) visits "
         f"FROM {storeName}_intermediate "
-        f"WHERE dt=DATE('{day}') "
+        f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata' "
         "GROUP BY mac "
         "HAVING COUNT(*) > 1 "
         "ORDER BY COUNT(*) DESC"
@@ -98,7 +99,7 @@ def averageVisitDurationInMinutes(storeName, day):
     query = (
         "SELECT avg(date_diff('minute', first_seen, last_seen)) duration "
         f"FROM {storeName}_intermediate "
-        f"WHERE dt=date('{day}')"
+        f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'daily_avg_duration', day)
     return executeQuery(query, outputLocation)
