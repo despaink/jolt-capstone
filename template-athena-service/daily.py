c3b6ae41bbf2c6c17e291d1e83bf12f96fd3898f
decongo <conger.devon@gmail.com>
2020-03-17 15:40:02 -0600

    all queries OpenCSVSerDe compliant

--- template-athena-service/daily.py
+++ template-athena-service/daily.py
@@ -14,10 +14,9 @@ def handle(event, context):
     responses = []
     responses.append(joinDailyRecords(storeName, day))
     responses.append(addPartition(storeName, day))
-    # responses.append(uniquePerHour(storeName, day))
-    # responses.append(totalUnique(storeName, day))
-    # responses.append(repeatByMac(storeName, day))
-    # responses.append(averageVisitDurationInMinutes(storeName, day))
+    responses.append(uniquePerHour(storeName, day))
+    responses.append(totalUnique(storeName, day))
+    responses.append(averageVisitDurationInMinutes(storeName, day))
     
     print(responses)
     return responses
@@ -61,11 +60,11 @@ def addPartition(storeName, day):
 
 def uniquePerHour(storeName, day):
     query = (
-        "SELECT date_trunc('hour', first_seen) time, Count(*) visits "
-		f"FROM {storeName} "
+        "SELECT date_trunc('hour', date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s')) time, Count(*) visits "
+        f"FROM {storeName} "
         f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata' "
-		"GROUP BY date_trunc('hour', first_seen) "
-		"ORDER BY date_trunc('hour', first_seen)"
+        "GROUP BY date_trunc('hour', date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s')) "
+        "ORDER BY date_trunc('hour', date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s'))"
     )
     outputLocation = constructOutputLocation(storeName, 'unique_per_hour', day)
     return executeQuery(query, outputLocation)
@@ -75,30 +74,17 @@ def uniquePerHour(storeName, day):
 def totalUnique(storeName, day):
     query = (
         "SELECT COUNT(DISTINCT mac) visits "
-        f"FROM {storeName}_intermediate "
+        f"FROM {storeName} "
         f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'daily_total_unique', day)
     return executeQuery(query, outputLocation)
 
 
-def repeatByMac(storeName, day):
-    query = (
-        "SELECT mac, COUNT(*) visits "
-        f"FROM {storeName}_intermediate "
-        f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata' "
-        "GROUP BY mac "
-        "HAVING COUNT(*) > 1 "
-        "ORDER BY COUNT(*) DESC"
-    )
-    outputLocation = constructOutputLocation(storeName, 'daily_repeat_by_mac', day)
-    return executeQuery(query, outputLocation)
-
-
 def averageVisitDurationInMinutes(storeName, day): 
     query = (
-        "SELECT avg(date_diff('minute', first_seen, last_seen)) duration "
-        f"FROM {storeName}_intermediate "
+        "SELECT avg(date_diff('minute', date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s'), date_parse(trim(last_seen), '%Y-%m-%d %H:%i:%s'))) duration "
+        f"FROM {storeName} "
         f"WHERE dt='{day}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'daily_avg_duration', day)
