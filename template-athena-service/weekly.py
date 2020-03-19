a9f9531e2d0a2574362e11262a3aa5ae21c348ef
decongo <conger.devon@gmail.com>
2020-03-19 15:10:18 -0600

    swtiched from iso week

--- template-athena-service/weekly.py
+++ template-athena-service/weekly.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 import logging
-from datetime import date
+from datetime import date, timedelta
 
 athena_client = boto3.client('athena')
 FIRST_SEEN = "date_parse(trim(first_seen), '%Y-%m-%d %H:%i:%s')"
@@ -12,13 +12,16 @@ def handle(event, context):
     # TODO: extract storeName from event
     storeName = 'heritage_15'
     day = date.today()
-
+    #day = "2020-03-8"
+    # day = ( date.today() - timedelta(1) ).strftime('%Y-%m-%d')
+    weekStart = ( day - timedelta(weeks=1) ).strftime('%Y-%m-%d')
+    weekEnd = ( day - timedelta(days=1) ).strftime('%Y-%m-%d')
     responses = []
     
-    responses.append(uniquePerDay(storeName, day))
-    responses.append(totalUnique(storeName, day))
-    responses.append(totalRepeat(storeName, day))
-    responses.append(averageVisitDurationInMinutes(storeName, day))
+    responses.append(uniquePerDay(storeName, day, weekStart, weekEnd))
+    responses.append(totalUnique(storeName, day, weekStart, weekEnd))
+    responses.append(totalRepeat(storeName, day, weekStart, weekEnd))
+    responses.append(averageVisitDurationInMinutes(storeName, day, weekStart, weekEnd))
     
     print(responses)
     return responses
@@ -42,11 +45,11 @@ def constructOutputLocation(storeName, queryName, day):
 # # # # # # # # # # # # # # # # # # 
 # Query functions
 # # # # # # # # # # # # # # # # # # 
-def uniquePerDay(storeName, day):
+def uniquePerDay(storeName, day, weekStart, weekEnd):
     query = (
         f"SELECT date({FIRST_SEEN}) day, Count(*) visits "
         f"FROM {storeName} "
-        f"WHERE week({FIRST_SEEN})=week(DATE('{day}')) AND \"$PATH\" NOT LIKE '%metadata' "
+        f"WHERE dt between '{weekStart}' and '{weekEnd}' AND \"$PATH\" NOT LIKE '%metadata' "
         f"GROUP BY date({FIRST_SEEN}) "
         f"ORDER BY date({FIRST_SEEN})"
     )
@@ -56,24 +59,24 @@ def uniquePerDay(storeName, day):
 
 
 # total number of unique mac addresses that have appeared during the scans
-def totalUnique(storeName, day): 
+def totalUnique(storeName, day, weekStart, weekEnd): 
     query = (
         "SELECT COUNT(DISTINCT mac) visits "
         f"FROM {storeName} "
-        f"WHERE week({FIRST_SEEN})=week(DATE('{day}')) AND \"$PATH\" NOT LIKE '%metadata'"
+        f"WHERE dt between '{weekStart}' and '{weekEnd}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'weekly_total_unique', day)
     return executeQuery(query, outputLocation)
 
 
 # This is repeat customers, not visits. May be worthwhile making the distinction later
-def totalRepeat(storeName, day):
+def totalRepeat(storeName, day, weekStart, weekEnd):
     query = (
         "SELECT COUNT(*) repeat_customers "
         "FROM ( "
             "SELECT mac, COUNT(*) visits "
             f"FROM {storeName} "
-            f"WHERE week({FIRST_SEEN})=week(DATE('{day}')) AND \"$PATH\" NOT LIKE '%metadata' "
+            f"WHERE dt between '{weekStart}' and '{weekEnd}' AND \"$PATH\" NOT LIKE '%metadata' "
             "GROUP BY mac "
             "HAVING COUNT(*) > 1 "
             "ORDER BY COUNT(*) DESC "
@@ -83,11 +86,11 @@ def totalRepeat(storeName, day):
     return executeQuery(query, outputLocation)
 
 
-def averageVisitDurationInMinutes(storeName, day):
+def averageVisitDurationInMinutes(storeName, day, weekStart, weekEnd):
     query = (
         f"SELECT avg(date_diff('minute', {FIRST_SEEN}, {LAST_SEEN})) duration "
         f"FROM {storeName} "
-        f"WHERE week({FIRST_SEEN})=week(date('{day}')) AND \"$PATH\" NOT LIKE '%metadata'"
+        f"WHERE dt between '{weekStart}' and '{weekEnd}' AND \"$PATH\" NOT LIKE '%metadata'"
     )
     outputLocation = constructOutputLocation(storeName, 'weekly_avg_duration', day)
     return executeQuery(query, outputLocation)
